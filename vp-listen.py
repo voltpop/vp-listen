212a500d4818d8cf6144b8fa84bc7ca437e04c20
dfoulks1 <dfoulks@apache.org>
2022-02-11 17:07:21 -0600

    added a timeout and re-check of 1 second until it starts hearing a response

--- vp-listen.py
+++ vp-listen.py
@@ -6,12 +6,14 @@ import pickle
 channel = "ansible"
 
 if __name__ == "__main__":
-    r = redis.Redis(host='localhost', port='6379', db='0')
+    r = redis.Redis(host='localhost', port='10001', db='0')
     p = r.pubsub()
     p.subscribe(channel)
     while True:
         t = p.get_message()
-        if t["type"] == "message":
-            print(pickle.loads(codecs.decode(t['data'], 'base64')))
-
-        time.sleep(15)
+        if t:
+            if t["type"] == "message":
+                print(pickle.loads(codecs.decode(t['data'], 'base64')))
+            time.sleep(15)
+        else:
+            time.sleep(1)
