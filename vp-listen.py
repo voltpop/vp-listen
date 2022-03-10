765a0440acbf1f51ba95d9e08722ddfd23676bb1
dfoulks1 <dfoulks@apache.org>
2022-03-10 11:41:37 -0600

    listener configuration file and service file added

--- vp-listen.py
+++ vp-listen.py
@@ -1,15 +1,24 @@
 import redis
 import time
-import codecs 
+import codecs
 import pickle
+import yaml
 
 channel = "ansible"
-
+redishost = "announce.voltpop.com"
+redisport = 10001
 if __name__ == "__main__":
-    r = redis.Redis(host='localhost', port='10001', db='0')
+    config = yaml.load(open('listen.yml', "r").read(), Loader=yaml.SafeLoader)["listener"]
+    print("Connecting to %s:%s"%(config['redis_host'], config['redis_port']))
+    r = redis.Redis(host=config['redis_host'], port=config["redis_port"], db='0')
     p = r.pubsub()
-    p.subscribe(channel)
+    
+    for channel in config["channels"]:
+        print("Subscribing to channel: %s" % (channel))
+        p.subscribe(channel)
+
     while True:
+        print("Checking Messages...")
         t = p.get_message()
         if t:
             if t["type"] == "message":
