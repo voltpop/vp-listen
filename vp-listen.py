66ae62aac8b84abf04829b6f06882f26912f24e3
dfoulks1 <dfoulks@apache.org>
2022-03-14 13:03:27 -0500

    Added action support and logging

--- vp-listen.py
+++ vp-listen.py
@@ -3,26 +3,37 @@ import time
 import codecs
 import pickle
 import yaml
+import pprint
+import subprocess
+import logging
 
-channel = "ansible"
-redishost = "announce.voltpop.com"
-redisport = 10001
+logging.basicConfig(level=logging.INFO)
+pp = pprint.PrettyPrinter()
+
 if __name__ == "__main__":
+    logging.info("Starting VoltPop Listener Service")
     config = yaml.load(open('listen.yml', "r").read(), Loader=yaml.SafeLoader)["listener"]
-    print("Connecting to %s:%s"%(config['redis_host'], config['redis_port']))
+    logging.info("Connecting to %s:%s"%(config['redis_host'], config['redis_port']))
     r = redis.Redis(host=config['redis_host'], port=config["redis_port"], db='0')
     p = r.pubsub()
     
     for channel in config["channels"]:
-        print("Subscribing to channel: %s" % (channel))
-        p.subscribe(channel)
+        logging.info("Subscribing to channel: %s" % (channel['name']))
+        p.subscribe(channel['name'])
 
     while True:
-        print("Checking Messages...")
         t = p.get_message()
-        if t:
-            if t["type"] == "message":
-                print(pickle.loads(codecs.decode(t['data'], 'base64')))
-            time.sleep(15)
+        if t and t["type"] == "message":
+            logging.info("Received message for %s" % t["channel"].decode('utf8'))
+            channel_config = next((channel for channel in config["channels"] if channel["name"] == t["channel"].decode('utf8')), None)
+            if channel_config["action"]:
+                if isinstance(channel_config['action'], str):
+                    subprocess.call(channel_config["action"].split())
+                elif isinstance(channel_config["action"], list):
+                    [ logging.info("%s" % subprocess.check_output(cmd.split())) for cmd in channel_config["action"] ]
+                else:
+                    logging.error("ERROR! in action statement: %s" % channel_config["action"])
+            #data = pickle.loads(codecs.decode(t['data'], 'base64'))
+            #logging.debug("%s" % str(data))
         else:
             time.sleep(1)
