67c3e762cd03d539419ae6d526e30d39cad95fd4
dfoulks1 <dfoulks@apache.org>
2023-05-25 15:54:44 -0500

    fixed actions for now

--- vp-listen.py
+++ vp-listen.py
@@ -8,13 +8,14 @@ import subprocess
 import logging
 import jinja2
 
-logging.basicConfig(level=logging.INFO)
+#logging.basicConfig(level=logging.INFO)
 pp = pprint.PrettyPrinter()
 
 if __name__ == "__main__":
     logging.info("Starting VoltPop Listener Service")
     config = yaml.load(open('listen.yml', "r").read(), Loader=yaml.SafeLoader)["listener"]
     logging.info("Connecting to %s:%s"%(config['redis_host'], config['redis_port']))
+    j2 = jinja2.Environment()
     r = redis.Redis(host=config['redis_host'], port=config["redis_port"], db='0')
     p = r.pubsub()
     
@@ -30,5 +31,16 @@ if __name__ == "__main__":
             data = pickle.loads(codecs.decode(t["data"], 'base64'))
             channel_config = next((channel for channel in config["channels"] if channel["name"] == t["channel"].decode('utf8')), None)
             logging.info("%s: %s" % (channel_name, str(data)))
+
+            # Actions handling
+            for channel in config["channels"]:
+                if channel["name"] == channel_name:
+                    c = channel
+            
+            if "action" in c.keys():
+                task = j2.from_string(c["action"]).render(data=str(data))
+                action = subprocess.Popen(['/bin/bash', '-c', task], stdout=subprocess.PIPE)
+                out, err = action.communicate()
+                print(out.decode('utf8').strip())
         else:
             time.sleep(1)
